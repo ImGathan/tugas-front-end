1424ade974a8785e34ff16774ef7d677620049a9
ImGathan <sigit010719@gmail.com>
2024-11-21 13:09:21 +0700

    new

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html"
+  ],
+  theme: {
+    extend: {
+      fontFamily : {
+        inter: "Inter",
+        poppins: "Poppins",
+        raleway: "Raleway",
+        cabin: "Cabin"
+      }
+    },
+  },
+  plugins: [],
+}
+
